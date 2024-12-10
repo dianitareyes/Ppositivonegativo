5c25fd6057dba8786d30e4654479056eed0eac89
Minorusal <minorusal@hotmail.com>
2024-12-10 14:48:31 -0600

    Se importa la libreria scanner, se hace la instancia de la clase scanner y se declaran las variables numeros y sumapositivos

---
+++ Ppositivonegativo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ppositivonegativo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int sumaPositivos = 0, sumaNegativos = 0, countPositivos = 0, countNegativos = 0;
+    }
+} 
