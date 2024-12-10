5a223cced99524fc68bc771e91116cb60e7a0567
Minorusal <minorusal@hotmail.com>
2024-12-10 14:55:34 -0600

    solicita 10 números enteros, valida q cada uno sea entero,clasifica cada uno como positivo o negativo y por ultimo acumula la suma y cuenta cuantos son positivos y negativos

--- Ppositivonegativo.java
+++ Ppositivonegativo.java
@@ -5,5 +5,23 @@ public class Ppositivonegativo {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[10];
         int sumaPositivos = 0, sumaNegativos = 0, countPositivos = 0, countNegativos = 0;
+
+         System.out.println("Ingrese 10 números enteros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            while (!scanner.hasNextInt()) { 
+                System.out.print("Entrada no válida. Intente nuevamente: ");
+                scanner.next();
+            }
+            numeros[i] = scanner.nextInt();
+
+            if (numeros[i] > 0) {
+                sumaPositivos += numeros[i];
+                countPositivos++;
+            } else if (numeros[i] < 0) {
+                sumaNegativos += numeros[i];
+                countNegativos++;
+            }
+        }
     }
 } 
